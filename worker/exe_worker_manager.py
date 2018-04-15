1fb16a49ceea40237d1a258239460f8a65072bdd
sfutagami <sbuta23dev@yahoo.co.jp>
2018-04-16 00:52:46 +0900

    軽微なコメント追加

--- worker/exe_worker_manager.py
+++ worker/exe_worker_manager.py
@@ -4,15 +4,19 @@
 import re
 import sys
 
-""" workermanager系を実行
+""" workermanager系実行スクリプト
 """
 
+# クラスのメタ情報取得
 task_name   = sys.argv[1]
 class_name  = task_name + "WorkerManager"
 dir_name    = re.sub("([A-Z])", lambda x:"_" + x.group(1).lower(), task_name)[1:]
 module_name = dir_name + "." + dir_name + "_worker_manager"
 
+# メタ情報からインスタンス作成
 module = __import__(module_name, fromlist=[class_name])
 worker_manager = getattr(module, class_name)
 wm = worker_manager()
+
+# 実処理記載
 wm.before()
