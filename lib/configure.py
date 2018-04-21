02e43758d333d6f551aa3cf18754bd3204d988a9
sfutagami <sbuta23dev@yahoo.co.jp>
2018-04-21 22:23:29 +0900

    設定ファイル読み込みクラス追加

---
+++ lib/configure.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import yaml
+
+class Configure(object):
+    """ 設定ファイルの値を管理するためのクラス
+    """
+    __config_dict = {}
+
+    def __init__(self):
+        print "Configure class is not init.(all static method)"
+
+    @staticmethod
+    def load_yaml(path):
+        """ yaml形式の設定ファイル読み込みを実行
+        :param path: string 読み込みファイルパス
+        """
+        # 設定ファイル内容をマージする
+        with open(path) as file:
+            cls.__config_dict.update(yaml.load(file))
+
+    @staticmethod
+    def get(key):
+        """ キーに該当する設定ファイルの値を取得
+        :param key: string 設定値取得キー
+        """
+        def __get(k, v):
+            if '.' in k:
+                _k = k[k.find('.')+1:]
+                _v = v[k.split('.')[0]]
+                return __get(_k, _v)
+            return v[k]
+
+        return __get(key, cls.__config_dict)
