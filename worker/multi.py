26022df9ce8dac1aad1f00ab5a90ce172afc9014
sfutagami <sbuta23dev@yahoo.co.jp>
2018-05-24 02:26:08 +0900

    子プロセス終了処理追加

--- worker/multi.py
+++ worker/multi.py
@@ -8,29 +8,65 @@ import sys
 import time
 import subprocess
 import multiprocessing as multi
+import logging
+import signal
 
 def main(argv):
     if len(argv) < 2:
         print 'Usage: [command [argv]]'
         exit(1)
 
+    mylogger = logging.getLogger(__name__)
+    # ログレベルの設定
+    mylogger.setLevel(10)
+    fh = logging.FileHandler('test.log')
+    fh.setFormatter(logging.Formatter("[%(levelname)s][%(pathname)s(%(lineno)s)]:%(message)s"))
+    mylogger.addHandler(fh)
+
+    def handler(signum, frame):
+        for p in multi.active_children():
+            mylogger.info("Terminate={}".format(p.pid))
+            p.terminate()
+        exit(0)
+
+    # Set the signal handler and a 5-second alarm
+    signal.signal(signal.SIGTERM, handler)
+
     def call_process():
         """ コマンドに設定したプロセスを実行
         """
-        subprocess.call(argv[1:])
+        try:
+            p = subprocess.Popen(argv[1:])
+            mylogger.info("Start={}".format(p.pid))
+            p.wait()
+        except:
+            mylogger.info("Terminate={}".format(p.pid))
+            p.terminate()
 
-    while True:
-        # 最大10回子プロセス起動
-        while len(multi.active_children()) < 3:
-            p = multi.Process(name='worker', target=call_process)
-            # 親プロセスが死んだら,子プロセスも死ぬようにする
-            p.daemon = True
-            p.start()
-            # 次のプロセス作成前に0.1秒待ち
-            time.sleep(0.1)
+        def handler(signum, frame):
+            mylogger.info("Terminate={}".format(p.pid))
+            p.terminate()
 
-        # 1秒毎にプロセス数確認
-        time.sleep(1)
+        signal.signal(signal.SIGTERM, handler)
+
+    try:
+        while True:
+            # 最大10回子プロセス起動
+            while len(multi.active_children()) < 3:
+                p = multi.Process(name='worker', target=call_process)
+                # 親プロセスが死んだら,子プロセスも死ぬようにする
+                # p.daemon = True
+                p.start()
+                mylogger.info("make process {}".format(p.pid))
+                # 次のプロセス作成前に0.1秒待ち
+                time.sleep(0.1)
+
+            # 1秒毎にプロセス数確認
+            time.sleep(1)
+    except:
+        for p in multi.active_children():
+            mylogger.info("Terminate={}".format(p.pid))
+            p.terminate()
 
 if __name__ == '__main__':
     main(sys.argv)
